946fb89c7fca964bbea4248d17134c69f4a32155
Timothy Horth <tjhorth@gmail.com>
2022-08-15 21:59:45 -0400

    Update to project

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   devtool: "inline-source-map",
   entry: {
-    main: "./src/pages/index.js",
+    main: "./docs/pages/index.js",
   },
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -47,7 +47,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: "./docs/index.html",
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
